a25dda824f75924533f5ce4698b728919f76bf69
PocoPota <pocopota.program@gmail.com>
2021-08-02 15:06:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+$(function () {
+  $('#sample1').raindrops();
+});
+$(function () {
+  $('#sample2').raindrops({
+    color: '#ff0000'
+  });
+});
+$(function () {
+  $('#sample3').raindrops({
+    waveLength: 30
+  });
+});
+$(function () {
+  $('#sample4').raindrops({
+    frequency: 800
+  });
+});
+$(function () {
+  $('#sample5').raindrops({
+    waveHeight: 1000,
+    canvasHeight: 180
+  });
+});
+$(function () {
+  $('#sample6').raindrops({
+    density: 0
+  });
+});
+$(function () {
+  $('#sample7').raindrops({
+    rippleSpeed: 0.5
+  });
+});
+$(function () {
+  $('#sample8').raindrops({
+    canvasWidth: 200
+  });
+});
+$(function () {
+  $('#sample9').raindrops({
+    canvasHeight: 20
+  });
+});
